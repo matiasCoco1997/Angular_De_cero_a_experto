b66dc52cf085e628399b15f141c756fab7fdea41
Matias Ariel Coco <cocomatias6@gmail.com>
2024-01-18 09:05:54 -0300

    update clase 15 terminada
    Explicacion de tipos de datos y principio de imports

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -2,6 +2,7 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
+// import './topics/01-basic-types.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
