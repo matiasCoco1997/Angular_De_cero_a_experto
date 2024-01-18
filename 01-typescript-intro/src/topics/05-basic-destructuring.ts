68c82fc27fabea21d9fd8d001e791270f92b02b3
Matias Ariel Coco <cocomatias6@gmail.com>
2024-01-18 13:55:49 -0300

    update desestructuracion de arrays

--- 01-typescript-intro/src/topics/05-basic-destructuring.ts
+++ 01-typescript-intro/src/topics/05-basic-destructuring.ts
@@ -29,13 +29,22 @@ const {
     details//desestructuramos el objeto detalles
 } = audioPlayer;
 
-const { author, year } = details; //desestructuramos el autor y el año
+const { author } = details; //desestructuramos el autor y el año
  
+/*
 console.log( "Song: " , anotherSong);
 console.log( "Song: " , song, "/ Duration: ", duration);
 console.log("Author: " , author, " / Year: ", year);
+*/
 
 
+
+const [p1, p2, trunks = "No existe el personaje"]: string[] = ["Goku", "Vegeta", "Trunks"];
+
+//const [ , , trunks = "No existe el personaje"]: string[] = ["Goku", "Vegeta", "Trunks"];
+//En el caso de solo querer acceder a trunks, solo se colocaran comas indicando que no nos interesan esos valores y solo queremos acceder a trunks
+
+console.log("Personaje 1: " , trunks );
 
 
 
