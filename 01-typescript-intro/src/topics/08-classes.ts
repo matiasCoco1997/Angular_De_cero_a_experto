c2d9c29cc97a235dcfd99d8c24e2ba8c88c3ce55
Matias Ariel Coco <cocomatias6@gmail.com>
2024-01-18 15:15:27 -0300

    update fin de clase constructor

--- 01-typescript-intro/src/topics/08-classes.ts
+++ 01-typescript-intro/src/topics/08-classes.ts
@@ -2,12 +2,11 @@
 
 export class Person {
 
-    public name?: string;
-    private address?: string;
+    constructor( 
+        public name: string, 
+        private address:string = "No address"
+        ){
 
-    constructor( name: string, address:string ){
-        this.name = name;
-        this.address = address;
     }
 
 }
