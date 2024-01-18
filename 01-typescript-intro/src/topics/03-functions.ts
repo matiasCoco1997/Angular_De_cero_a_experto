b4a129c5fd7a5a0f9e818f1c3e012521156ab196
Matias Ariel Coco <cocomatias6@gmail.com>
2024-01-18 11:12:15 -0300

    update funciones con objetos
    explicación básica de funciones con objetos en Typescript

--- 01-typescript-intro/src/topics/03-functions.ts
+++ 01-typescript-intro/src/topics/03-functions.ts
@@ -17,13 +17,43 @@ function multiply ( firstNumber: number, secondNumber?: number, base: number = 2
 }
 
 
-const result: number = addNumbers(1, 2);
+/*const result: number = addNumbers(1, 2);
 showResult(result);
 
 const result2: string = addNumbersArrow(3, 4);
-showResult(result);
+showResult(result2);
 
 const multiplyResult: number = multiply(5);
-showResult(multiplyResult);
+showResult(multiplyResult);*/
+
+interface Character {
+    name: string;
+    hp: number;
+    showHp: () => void;
+}
+
+const personaje: Character = {
+    name: "Aragorn",
+    hp: 50,
+    showHp() {
+        console.log(`Puntos de vida ${ this.hp }`);
+    }
+}
+
+
+const healCharacter = ( character: Character , amount:number ) => {
+
+    if( character.hp+amount > 100 ){
+        character.hp = 100;
+    } else{
+        character.hp += amount;
+    }
+}
+
+personaje.showHp();
+
+healCharacter(personaje, 100);
+
+personaje.showHp();
 
 export {};
