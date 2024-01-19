29d06a417295889d041e20c3efb548fdf8fc58e4
Matias Ariel Coco <cocomatias6@gmail.com>
2024-01-19 14:20:57 -0300

    update funcionalidad contador

--- 02-bases/src/app/counter/counter.component.ts
+++ 02-bases/src/app/counter/counter.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
 
-@Component( {
+@Component({
   selector: 'app-counter',
-  template: '<h1>./app-counter.component.html</h1>'
+  template: `
+              <h1>Hola Counter</h1>
+
+              <h3>Counter: {{counter}}</h3>
+
+              <button (click)="increaseBy(1)">+1</button>
+              <button (click)="dicreaseBy(1)">-1</button>
+              <button (click)="reset()">Reset</button>
+            `
 })
 
+export class CounterComponent  {
 
+  constructor() { }
+  public counter: number = 10;
 
-export class CounterComponent {
+  increaseBy( value : number ):void {
+    this.counter += value;
+  }
+
+  dicreaseBy( value : number ):void {
+    this.counter -= value;
+  }
+
+  reset () : void{
+    this.counter = 10;
+  }
 
 }
