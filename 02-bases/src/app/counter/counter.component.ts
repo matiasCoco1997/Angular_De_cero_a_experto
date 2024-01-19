db0b800f6a3f884aa4b29511a3994ecf03d14d80
Matias Ariel Coco <cocomatias6@gmail.com>
2024-01-19 14:08:09 -0300

    update componente contador

---
+++ 02-bases/src/app/counter/counter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component( {
+  selector: 'app-counter',
+  template: '<h1>./app-counter.component.html</h1>'
+})
+
+
+
+export class CounterComponent {
+
+}
