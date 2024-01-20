2e0a36744b5c72c998234e75bc2af96ebe4c1fe5
Matias Ariel Coco <cocomatias6@gmail.com>
2024-01-20 20:09:41 -0300

    update fin de clase creando componentes pequeños

---
+++ 02-bases/src/app/dbz/components/character/character.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dbz-add-character',
+  templateUrl: './character.component.html',
+  styleUrl: './character.component.css'
+})
+export class AddCharacterComponent {
+
+}
