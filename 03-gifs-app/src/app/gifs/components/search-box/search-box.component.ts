9b8ff51694d409f3fcfc114668982cb7a799b854
Matias Ariel Coco <cocomatias6@gmail.com>
2024-01-22 13:38:33 -0300

    update viewChild

--- 03-gifs-app/src/app/gifs/components/search-box/search-box.component.ts
+++ 03-gifs-app/src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'gifs-search-box',
@@ -9,4 +9,12 @@ import { Component } from '@angular/core';
 
 export class SearchBoxComponent {
 
+  @ViewChild("txtTagInput", )
+  public tagInput!: ElementRef<HTMLInputElement>;
+
+  searchTag():void{
+    const newTag = this.tagInput.nativeElement.value;
+
+    console.log({newTag});
+  }
 }
