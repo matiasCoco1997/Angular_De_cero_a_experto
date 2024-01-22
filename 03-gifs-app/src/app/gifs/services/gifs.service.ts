588971152ff023e68aa5dd96366866f584d323ab
Matias Ariel Coco <cocomatias6@gmail.com>
2024-01-22 14:44:35 -0300

    update generando la api key

--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
   private _tagsHistory: string[] = [];
+  private apiKey: string = "xaxndMvmrqLEZeXMleYuoP5YMnL4mLG2";
 
   constructor() { }
 
