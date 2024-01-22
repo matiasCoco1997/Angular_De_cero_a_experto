74ab1e2d82f51431791bdeb4a0827df8475921f0
Matias Ariel Coco <cocomatias6@gmail.com>
2024-01-22 14:33:40 -0300

    update aplicando validaciones al servicio

--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -14,6 +14,22 @@ export class GifsService {
   }
 
   searchTag( tag: string):void {
+
+    if(tag.length === 0) return;
+
+    this.organizeHistory(tag);
+
     this._tagsHistory.unshift(tag);
+  }
+
+  private organizeHistory(tag:string):void{
+
+    tag = tag.toLowerCase();
+
+    if( this._tagsHistory.includes(tag) ){
+      this._tagsHistory = this._tagsHistory.filter( (oldTag) => oldTag !== tag ); //borro el tag existente
+    }
+
+    this._tagsHistory = this.tagsHistory.splice(0, 10); //lo reducimos a las ultimas 10 busquedas
   }
 }
