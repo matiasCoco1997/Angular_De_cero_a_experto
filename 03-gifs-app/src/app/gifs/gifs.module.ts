ee12dc2025b915eda08b591a75af24ce49b2e0f9
Matias Ariel Coco <cocomatias6@gmail.com>
2024-01-22 13:28:00 -0300

    update creando cards para los gifs

--- 03-gifs-app/src/app/gifs/gifs.module.ts
+++ 03-gifs-app/src/app/gifs/gifs.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './pages/home/home.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { CardListComponent } from './components/card-list/card-list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomePageComponent,
+    SearchBoxComponent,
+    CardListComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    HomePageComponent,
+    SearchBoxComponent,
+    CardListComponent
   ]
 })
 export class GifsModule { }
