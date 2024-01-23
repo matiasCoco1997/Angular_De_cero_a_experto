f5183e47c8ed84e4f29c66696877d729bbf705c8
Matias Ariel Coco <cocomatias6@gmail.com>
2024-01-23 09:41:58 -0300

    update loader creado

--- 03-gifs-app/src/app/shared/components/lazy-image/lazy-image.component.ts
+++ 03-gifs-app/src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'shared-lazy-image',
   templateUrl: './lazy-image.component.html'
 })
 
-export class LazyImageComponent {
+export class LazyImageComponent implements OnInit {
 
+  public hasLoaded: boolean = false;
 
+  @Input()
+  public url!: string;
+
+  @Input()
+  public alt: string = "No title";
+
+  ngOnInit(): void {
+    if( !this.url ){
+      throw new Error("Url property is required.");
+    }
+  }
+
+  onLoad():void{
+    setTimeout( () => {
+      this.hasLoaded = true;
+    }, 500);
+  }
 
 }
