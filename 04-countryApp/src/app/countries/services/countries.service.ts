286d4512e71ee60c35bc9788288c024a55557884
Matias Ariel Coco <cocomatias6@gmail.com>
2024-01-25 08:31:52 -0300

    update refactorización del servicio

--- 04-countryApp/src/app/countries/services/countries.service.ts
+++ 04-countryApp/src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, map, of, } from 'rxjs';
+import { Observable, catchError, delay, map, of, } from 'rxjs';
 import { Country } from '../interfaces/country.interface';
 
 @Injectable({providedIn: 'root'})
@@ -14,14 +14,13 @@ export class CountriesService {
   searchCuntryByAlphaCode( code: string) : Observable <Country | null>{
     //https://restcountries.com/v3.1/alpha/{code}
 
-    const typeOfSearch: string = `/alpha/`;
-
     const url = `${this.apiUrl}/alpha/${code}`;
 
     return this.http.get<Country[]>(url)
     .pipe(
       map( countries => countries.length > 0 ? countries[0] : null) ,
-      catchError( () => of( null ))
+      catchError( () => of( null )),
+      delay(2000), //genera un delay de 2 segundos para que carguen los elementos
     );
   }
 
@@ -29,14 +28,14 @@ export class CountriesService {
     //https://restcountries.com/v3.1/capital/{capital}
     const typeOfSearch: string = `/capital/`;
 
-    return this.search(typeOfSearch, term);
+    return this.getCountriesRequest(typeOfSearch, term);
   }
 
   searchByCountry( term: string) : Observable <Country[]>{
     //https://restcountries.com/v3.1/name/{name}
     const typeOfSearch: string = `/name/`;
 
-    return this.search(typeOfSearch, term);
+    return this.getCountriesRequest(typeOfSearch, term);
   }
 
   searchByRegion( term: string) : Observable <Country[]>{
@@ -44,11 +43,11 @@ export class CountriesService {
 
     const typeOfSearch: string = `/region/`;
 
-    return this.search(typeOfSearch, term);
+    return this.getCountriesRequest(typeOfSearch, term);
   }
 
 
-  private search(type:string, term:string):Observable <Country[]>{
+  private getCountriesRequest(type:string, term:string):Observable <Country[]>{
 
     const searchUrl: string = `${ this.apiUrl }${type}${term}`;
 
