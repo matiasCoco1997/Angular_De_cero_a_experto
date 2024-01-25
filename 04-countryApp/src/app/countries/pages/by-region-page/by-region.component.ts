bc0f3439099492bb51e9034ceae844a596d1c4c1
Matias Ariel Coco <cocomatias6@gmail.com>
2024-01-25 14:49:08 -0300

    update mejoras en filtrado por region

--- 04-countryApp/src/app/countries/pages/by-region-page/by-region.component.ts
+++ 04-countryApp/src/app/countries/pages/by-region-page/by-region.component.ts
@@ -2,24 +2,32 @@ import { Component } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country.interface';
 
+type Region = "Africa" | "Americas" | "Asia" | "Europe" | "Oceania";
 @Component({
   selector: 'app-by-region',
   templateUrl: './by-region.component.html',
   styles: ``
 })
+
 export class ByRegionPageComponent {
 
   public countries:Country[] = [];
+
+  public regions: Region[] = ["Africa", "Americas", "Asia", "Europe", "Oceania"];
+
+  public selectedRegion?: Region;
 
   public isLoading: boolean = false;
 
   constructor(private countriesService:CountriesService){}
 
-  searchByRegion( term: string):void{
+  searchByRegion( region: Region):void{
 
     this.isLoading = true;
 
-    this.countriesService.searchByRegion(term)
+    this.selectedRegion = region;
+
+    this.countriesService.searchByRegion(region)
     .subscribe( countries => {
       this.countries = countries;
       this.isLoading = false;
