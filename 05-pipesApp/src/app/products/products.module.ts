c20f35a58c5127adc7ae5b97150e1a718b1660b8
Matias Ariel Coco <cocomatias6@gmail.com>
2024-01-31 13:42:57 -0300

    update creando el toggleCasePipe

--- 05-pipesApp/src/app/products/products.module.ts
+++ 05-pipesApp/src/app/products/products.module.ts
@@ -7,6 +7,7 @@ import { NumbersPageComponent } from './pages/numbers-page/numbers-page.componen
 import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 import { OrderComponent } from './pages/order/order.component';
+import { ToggleCasePipe } from './pipes/toogle-case.pipe';
 
 
 @NgModule({
@@ -14,7 +15,10 @@ import { OrderComponent } from './pages/order/order.component';
     BasicsPageComponent,
     NumbersPageComponent,
     UncommonPageComponent,
-    OrderComponent
+    OrderComponent,
+
+    //Pipes
+    ToggleCasePipe
   ],
   imports: [
     CommonModule,
