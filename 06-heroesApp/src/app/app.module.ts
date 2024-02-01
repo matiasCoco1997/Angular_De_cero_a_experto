865b509c240d31f74bdd380c6266bde2515e9156
Matias Ariel Coco <cocomatias6@gmail.com>
2024-02-01 11:08:04 -0300

    update instalaciones
    Instalación de Angular Material y PrimeFlex

--- 06-heroesApp/src/app/app.module.ts
+++ 06-heroesApp/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+
 
 @NgModule({
   declarations: [
@@ -15,7 +17,8 @@ import { SharedModule } from './shared/shared.module';
     SharedModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
