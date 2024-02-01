bb3d9f526463ed2709a49d143da42baf916f9109
Matias Ariel Coco <cocomatias6@gmail.com>
2024-02-01 10:14:09 -0300

    update creación de rutas pares e hijas con lazyloading

--- 06-heroesApp/src/app/app-routing.module.ts
+++ 06-heroesApp/src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
+import { AuthModule } from './auth/auth.module';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"auth",
+    loadChildren: () => import ("./auth/auth.module").then( module => module.AuthModule),
+  },
+
+  {
+    path:"heroes",
+    loadChildren: () => import ("./heroes/heroes.module").then( module => module.HeroesModule),
+  },
+
+  {
+    path:"404",
+    component: Error404PageComponent
+  },
+
+  {
+    path:"",
+    redirectTo:"heroes",
+    pathMatch:"full"
+  },
+
+  {
+    path:"**",
+    redirectTo:"404"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- 06-heroesApp/src/app/heroes/heroes-routing.module.ts
+++ 06-heroesApp/src/app/heroes/heroes-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { NewPageComponent } from './pages/new-page/new-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",
+    component: LayoutPageComponent,
+    children: [
+      {
+        path:"new-hero",
+        component:NewPageComponent
+      },
+      {
+        path:"list",
+        component:ListPageComponent
+      },
+
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
