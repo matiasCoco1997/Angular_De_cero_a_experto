471538b9aae3e8a72f31bd2526cfb73dd22773f5
Matias Ariel Coco <cocomatias6@gmail.com>
2024-02-02 12:32:06 -0300

    update vista de héroe en específico

--- 06-heroesApp/src/app/heroes/pages/hero-page/hero-page.component.ts
+++ 06-heroesApp/src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { Hero } from '../../interfaces/hero.interface';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+
 
 @Component({
   selector: 'app-hero-page',
-  templateUrl: './hero-page.component.html',
-  styles: ``
+  templateUrl: './hero-page.component.html'
 })
-export class HeroPageComponent {
+export class HeroPageComponent implements OnInit{
+
+  public hero?: Hero;
+
+
+  constructor(
+    private _heroesService: HeroesService,
+    private ActivatedRoute: ActivatedRoute,
+    private router: Router
+  ){}
+
+
+  ngOnInit(): void {
+
+    this.ActivatedRoute.params.
+    pipe(
+      switchMap( ({ id }) => this._heroesService.getHeroById(id) )
+    ). subscribe( hero => {
+
+      if( !hero ){
+        //en el caso de que el heroe no exista, redirije hacia el listado de heroes
+        return this.router.navigateByUrl( "/heroes/list" )
+      }
+
+      this.hero = hero;
+      return;
+    })
+
+  }
+
+
 
 }
