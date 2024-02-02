f39310900ede17bb585a4bf1f322a94118bc644b
Matias Ariel Coco <cocomatias6@gmail.com>
2024-02-02 16:32:30 -0300

    update CRUD heroes
    Explicación de que es un CRUD y creación de métodos

--- 06-heroesApp/src/app/heroes/services/heroes.service.ts
+++ 06-heroesApp/src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from '../../../environments/environments';
 
@@ -26,6 +26,28 @@ export class HeroesService {
 
   getSuggestions( query: string ): Observable<Hero[]>{
     return this.http.get<Hero[]>(`${ this.baseUrl }/heroes?q=${ query }&_limit=6`);
+  }
+
+  addHer( hero:Hero ):Observable<Hero>{
+    //El primer argumento es la ruta y el segundo el parametro el cual se agregará
+    return this.http.post<Hero>(`${this.baseUrl}/heroes`, hero);
+  }
+
+  updateHero( hero:Hero ):Observable<Hero>{
+
+    if( !hero.id ){
+      throw Error ("Hero id is required.");
+    }
+    return this.http.patch<Hero>(`${this.baseUrl}/heroes/${ hero.id }`, hero);
+  }
+
+  deleteHeroById( id:string ):Observable<Boolean>{
+    //El primer argumento es la ruta y el segundo el parametro el cual se agregará
+    return this.http.delete(`${this.baseUrl}/heroes/${ id }`)
+    .pipe(
+      catchError( error => of(false)),
+      map( response => true )
+    );
   }
 
 }
