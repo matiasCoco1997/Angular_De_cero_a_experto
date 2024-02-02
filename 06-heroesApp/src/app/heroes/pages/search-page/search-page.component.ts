16e4330592042d5e85442afa9b1a4323e6921427
Matias Ariel Coco <cocomatias6@gmail.com>
2024-02-02 14:48:08 -0300

    update autocomplete parte 2 finalizado

--- 06-heroesApp/src/app/heroes/pages/search-page/search-page.component.ts
+++ 06-heroesApp/src/app/heroes/pages/search-page/search-page.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { HeroesService } from '../../services/heroes.service';
 import { FormControl } from '@angular/forms';
+import { Hero } from '../../interfaces/hero.interface';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-search-page',
@@ -10,7 +12,30 @@ import { FormControl } from '@angular/forms';
 export class SearchPageComponent {
 
   public searchInput = new FormControl("");
+  public heroes: Hero[] = [];
+  public selectedHero?: Hero;
 
   constructor( private _heroesService: HeroesService){}
+
+  searchHero():void{
+
+    const value: string = this.searchInput.value || "";
+
+    this._heroesService.getSuggestions(value)
+    .subscribe( heroes => this.heroes = heroes );
+  }
+
+  onSelectedOption( event: MatAutocompleteSelectedEvent ):void{
+
+    if( !event ){
+      this.selectedHero = undefined;
+      return;
+    }
+
+    const hero: Hero = event.option.value;
+    this.searchInput.setValue( hero.superhero );
+    this.selectedHero = hero;
+
+  }
 
 }
