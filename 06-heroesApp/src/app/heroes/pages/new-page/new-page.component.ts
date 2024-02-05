9aa15629eaf2be8c2fe7115026907d8c8e75a8c0
Matias Ariel Coco <cocomatias6@gmail.com>
2024-02-05 09:50:08 -0300

    update creación de getter y mostrar img

--- 06-heroesApp/src/app/heroes/pages/new-page/new-page.component.ts
+++ 06-heroesApp/src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { Publisher } from '../../interfaces/hero.interface';
+import { Hero, Publisher } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-new-page',
@@ -24,11 +25,19 @@ export class NewPageComponent {
     { id: "Marvel Comics", desc: "Marvel - Comics" }
   ];
 
+  constructor( private _heroService: HeroesService){}
+
+  get currentHero(): Hero{
+    const hero = this.heroForm.value as Hero;
+
+    return hero;
+  }
+
 
   onSubmit():void{
-    console.log({
-      formIsValid: this.heroForm.valid,
-      value: this.heroForm.value
-    });
+
+    if( !this.heroForm.valid ) return;
+
+    //this._heroService.updateHero( this.heroForm.value );
   }
 }
