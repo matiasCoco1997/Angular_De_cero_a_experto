a6b108ffdf61e8482bc8b6cf4cd5d06613999727
Matias Ariel Coco <cocomatias6@gmail.com>
2024-02-06 09:45:51 -0300

    update implementando el guard
    Validación de usuario

--- 06-heroesApp/src/app/auth/guards/auth.guard.ts
+++ 06-heroesApp/src/app/auth/guards/auth.guard.ts
@@ -1,21 +1,37 @@
-import { ActivatedRouteSnapshot, Route, UrlSegment, RouterStateSnapshot, CanMatchFn, CanActivateFn} from '@angular/router';
+import { ActivatedRouteSnapshot, Route, UrlSegment, RouterStateSnapshot, CanMatchFn, CanActivateFn, Router} from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { inject } from '@angular/core';
+import { Observable, tap } from 'rxjs';
 
-export const canActivateGuard: CanActivateFn = (
-  route: ActivatedRouteSnapshot,
-  state: RouterStateSnapshot
-) => {
-  console.log('CanActivate');
-  console.log({ route, state });
 
-  return true;
-};
+const checkAuthStatus = (): Observable<boolean> => {
+  const authService: AuthService = inject(AuthService);
+  const router: Router = inject(Router);
+
+  return authService.checkAuthentication().pipe(
+    tap( ( isAuthenticated:boolean ) => {
+      if( !isAuthenticated ){
+        router.navigateByUrl("/auth/login");
+      }
+    })
+  );
+}
 
 export const canMatchGuard: CanMatchFn = (
   route: Route,
   segments: UrlSegment[]
 ) => {
-  console.log('CanMatch');
-  console.log({ route, segments });
+  // console.log('CanMatch');
+  // console.log({ route, segments });
 
-  return true;
+  return checkAuthStatus();
+};
+
+export const canActivateGuard: CanActivateFn = (
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
+) => {
+  // console.log('CanActivate');
+  // console.log({ route, state });
+  return checkAuthStatus();
 };
