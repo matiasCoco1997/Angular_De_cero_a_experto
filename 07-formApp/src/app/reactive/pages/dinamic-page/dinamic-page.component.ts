328c62d3f67bb5a5c597944d9156af2e41ab69c8
Matias Ariel Coco <cocomatias6@gmail.com>
2024-02-07 15:42:58 -0300

    update clase de formularios dinamicos

--- 07-formApp/src/app/reactive/pages/dinamic-page/dinamic-page.component.ts
+++ 07-formApp/src/app/reactive/pages/dinamic-page/dinamic-page.component.ts
@@ -1,9 +1,39 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  templateUrl: './dinamic-page.component.html',
-  styles: ``
+  templateUrl: './dinamic-page.component.html'
 })
 export class DinamicPageComponent {
+
+
+  // public myFormTwo: FormGroup = this.fb.group({
+  //   name:
+  // });
+
+  public myForm: FormGroup = this.fb.group({
+    name:['', [Validators.required, Validators.minLength(3) ], ],
+    favoriteGames: this.fb.array([
+      ["Metal Gear", Validators.required],
+      ["Street Fighter", Validators.required],
+    ]),
+  });
+
+  constructor( private fb: FormBuilder ){}
+
+  onSubmit():void {
+
+    if( this.myForm.invalid ){
+      return;
+    }
+
+    console.log(this.myForm.value);
+
+    this.myForm.reset()
+  }
+
+  get favoriteGames(): FormArray{
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
 
 }
