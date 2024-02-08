27736eb889b252e6579cc2677268da85226d28eb
Matias Ariel Coco <cocomatias6@gmail.com>
2024-02-08 10:45:11 -0300

    update seteando el formulario html con el componente

--- 07-formApp/src/app/auth/pages/register-page/register-page.component.ts
+++ 07-formApp/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,34 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Passenger } from '../../../../../../01-typescript-intro/src/topics/11-optional-chaining';
 
 @Component({
-  templateUrl: './register-page.component.html',
-  styles: ``
+  templateUrl: './register-page.component.html'
 })
 export class RegisterPageComponent {
+
+  public myForm: FormGroup = this.fb.group({
+
+    name: ['', [ Validators.required ] ],
+    email: ['', [ Validators.required, Validators.email ] ],
+    username: ['', [ Validators.required ] ],
+    password: ['', [ Validators.required, Validators.minLength(6) ] ],
+    passwordConfirm: ['', [ Validators.required ] ],
+
+
+  });
+
+  constructor( private fb: FormBuilder ){}
+
+  isValidField( field:string ):boolean{
+    //TODO: obtener validator desde un servicio
+    return false;
+  }
+
+  onSubmit():void {
+
+    this.myForm.markAllAsTouched();
+
+  }
 
 }
