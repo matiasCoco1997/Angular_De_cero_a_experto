075ee949570971e52773f8d8b6d8f4c99b8a7fca
Matias Ariel Coco <cocomatias6@gmail.com>
2024-02-08 10:13:10 -0300

    update tarea finalizada

--- 07-formApp/src/app/reactive/pages/switches-page/switches-page.component.ts
+++ 07-formApp/src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,9 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  templateUrl: './switches-page.component.html',
-  styles: ``
+  templateUrl: './switches-page.component.html'
 })
-export class SwitchesPageComponent {
+export class SwitchesPageComponent implements OnInit{
 
+
+  public myForm: FormGroup = this.fb.group({
+    gender: ["M", [ Validators.required ] ],
+    wantNotifications: [ true, [ Validators.required ] ],
+    termsAndConditions: [ false, [ Validators.requiredTrue ] ],
+  });
+
+  public person = {
+    gender: "F",
+    wantNotifications: false
+  }
+
+  constructor( private fb: FormBuilder ) {}
+
+  ngOnInit(): void {
+    this.myForm.reset(this.person);
+  }
+
+  isValidField( field: string ):boolean | null {
+
+    return  this.myForm.controls[field].errors
+            &&
+            this.myForm.controls[field].touched;
+  }
+
+  onSave():void{
+
+    if( this.myForm.invalid ){
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    //Desestructuramos la opcion de terminos y condiciones,
+    //guardamos las demas propiedades por una persona
+    const { termsAndConditions, ...newPerson } = this.myForm.value;
+
+    this.person = newPerson;
+
+    console.log("Formulario enviado correctamente.");
+    console.log(this.person);
+  }
 }
