6434d877825815124d96b50eeb608dda6bc7f6ae
Matias Ariel Coco <cocomatias6@gmail.com>
2024-02-08 11:52:56 -0300

    update validador async

--- 07-formApp/src/app/auth/pages/register-page/register-page.component.ts
+++ 07-formApp/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ValidatorsService } from '../../../shared/services/validators.service';
+import { EmailValitadorService } from '../../../shared/validators/email-validator.service';
 
 
 @Component({
@@ -10,7 +11,7 @@ export class RegisterPageComponent {
 
   public myForm: FormGroup = this.fb.group({
     name: ['', [ Validators.required, Validators.pattern(this._validatorsService.firstNameAndLastnamePattern) ] ],
-    email: ['', [ Validators.required, Validators.pattern(this._validatorsService.emailPattern) ] ],
+    email: ['', [ Validators.required, Validators.pattern(this._validatorsService.emailPattern) ], [ this._emailValidatorService ] ],
     username: ['', [ Validators.required, this._validatorsService.cantBeStrider ] ],
     password: ['', [ Validators.required, Validators.minLength(6) ] ],
     passwordConfirm: ['', [ Validators.required ] ],
@@ -18,7 +19,8 @@ export class RegisterPageComponent {
 
   constructor(
     private fb: FormBuilder,
-    private _validatorsService: ValidatorsService ){}
+    private _validatorsService: ValidatorsService ,
+    private _emailValidatorService: EmailValitadorService ){}
 
   isValidField( field:string ):boolean | null{
     return this._validatorsService.isValidField( field, this.myForm );
