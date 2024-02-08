22448aff6221b687cf05cf457648d8b3c7375a25
Matias Ariel Coco <cocomatias6@gmail.com>
2024-02-08 11:15:52 -0300

    update validación de email y nombre

--- 07-formApp/src/app/auth/pages/register-page/register-page.component.ts
+++ 07-formApp/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { cantBeStrider } from '../../../shared/validators/validators';
+import * as customValidators from '../../../shared/validators/validators';
+
+
 
 @Component({
   templateUrl: './register-page.component.html'
@@ -9,9 +11,9 @@ export class RegisterPageComponent {
 
   public myForm: FormGroup = this.fb.group({
 
-    name: ['', [ Validators.required ] ],
-    email: ['', [ Validators.required, Validators.email ] ],
-    username: ['', [ Validators.required , cantBeStrider ] ],
+    name: ['', [ Validators.required, Validators.pattern(customValidators.firstNameAndLastnamePattern) ] ],
+    email: ['', [ Validators.required, Validators.pattern(customValidators.emailPattern) ] ],
+    username: ['', [ Validators.required, customValidators.cantBeStrider ] ],
     password: ['', [ Validators.required, Validators.minLength(6) ] ],
     passwordConfirm: ['', [ Validators.required ] ],
 
